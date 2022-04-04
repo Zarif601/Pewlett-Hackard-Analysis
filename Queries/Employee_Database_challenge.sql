720a6408987cefa95e9b540562fe9adf9c3523b1
Shadman Noor <51760888+Zarif601@users.noreply.github.com>
2022-04-04 16:40:05 -0700

    Added additional queries.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -49,3 +49,29 @@ ON (e.emp_no = t.emp_no)
 WHERE (de.to_date = '9999-01-01')
 AND (e.birth_date BETWEEN '01/01/1965' AND '12/31/1965')
 ORDER BY e.emp_no;
+
+
+-- Additional queries
+-- How many roles will need to be filled per department
+SELECT d.dept_name,
+	COUNT(d.dept_name)
+FROM unique_titles AS ut
+INNER JOIN dept_emp AS de
+ON (ut.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+GROUP BY d.dept_name
+ORDER BY count DESC;
+
+-- Are there enough qualified, retirement-ready employees to be mentors
+SELECT d.dept_name,
+	ut.title,
+	COUNT(d.dept_name)
+FROM unique_titles AS ut
+INNER JOIN dept_emp AS de
+ON (ut.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE (ut.title IN ('Manager', 'Technique Leader', 'Senior Staff', 'Senior Engineer'))
+GROUP BY d.dept_name, ut.title
+ORDER BY count DESC;
